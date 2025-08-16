a03db189e139e40c8e5e91c0b4ee8cbcdca38a96
pg-ui <pratham@prathamgupta.in>
2025-08-16 14:53:03 +0000

    N/A

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c369b36ab85abdb52d44eee2fa9ab6c8063442e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2791ed1000feb086239affa2bd5f6edab3c3707d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
